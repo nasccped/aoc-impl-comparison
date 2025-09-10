2372c731b9aff560cbf1b1ccf0300923b5e36103
Nasccped <pedro.main.tec@gmail.com>
2025-09-10 16:29:39 -0300

    chore: create main func program

---
+++ c/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "aoc_testing.h"
+#include "aoc_common.h"
+
+static char i01[] =
+#include "../inputs/day-01.c.txt"
+;
+
+int tests();
+
+int main(int argc, char *argv[]) {
+    if (argc > 2) {
+        printf("No action for this amount of args (%d)\n", argc);
+        printf("Aborting...\n");
+        return 1;
+    }
+    // skip if no args (goto main body)
+    else if (argc < 2);
+    else if (!strcmp(argv[1], "test")) return tests();
+    else {
+        printf("Non valid arg: %s\n", argv[1]);
+        return 1;
+    }
+    printf("Result for day 1 input: %lld", day_01_part_01(i01));
+    return 0;
+}
+
+int tests() {
+    int result = 1;
+    result &= day_01_tests();
+    return !result;
+}
