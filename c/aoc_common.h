eb0d87c5b7d6cd9a9e1209e226522a338274864a
Nasccped <pedro.main.tec@gmail.com>
2025-09-10 16:29:10 -0300

    chore: add common refers to global funcs

---
+++ c/aoc_common.h
@@ -0,0 +1,10 @@
+#ifndef AOC_COMMON_H
+#define AOC_COMMON_H
+
+/* Common functions to share between c files */
+
+usize day_01_part_01(char *input);
+usize day_01_part_02(char *input);
+int day_01_tests();
+
+#endif
